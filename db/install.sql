d47aa843e35f0813ad85d14844acb342e651f01e
Christian Flothmann <cflothma@uos.de>
2012-10-09 12:21:26 +0200

    add database installation and un-installation files

---
+++ db/install.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS `scorm_learning_units` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `cid` VARCHAR(32) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `introduction_text` TEXT,
+    `filename` VARCHAR(255) NOT NULL,
+    `scorm_version` VARCHAR(9) NOT NULL,
+    `starttime` DATETIME,
+    `endtime` DATETIME,
+    `popup` TINYINT(1) DEFAULT 1,
+    `grademethod` ENUM('count', 'max', 'average', 'sum'),
+    `maxgrade` INTEGER NOT NULL DEFAULT 100,
+    `maxattempt` INTEGER NOT NULL DEFAULT 0,
+    `whatgrade` ENUM('best', 'average', 'first', 'last')
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `scorm_scos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `learning_unit_id` INTEGER NOT NULL,
+    `manifest` VARCHAR(255) NOT NULL,
+    `organization` VARCHAR(255) NOT NULL,
+    `parent` VARCHAR(255) NOT NULL,
+    `identifier` VARCHAR(255) NOT NULL,
+    `launch` VARCHAR(255) NOT NULL,
+    `scormtype` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(255) NOT NULL
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `scorm_sco_data` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `sco_id` INTEGER NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `value` TEXT NOT NULL
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `scorm_sco_tracks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` VARCHAR(32) NOT NULL,
+    `learning_unit_id` INTEGER NOT NULL,
+    `sco_id` INTEGER NOT NULL,
+    `attempt` INTEGER NOT NULL,
+    `element` VARCHAR(255) NOT NULL,
+    `value` TEXT NOT NULL,
+    `timemodified` TIMESTAMP
+);
---
+++ db/uninstall.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `scorm_learning_units`;
+DROP TABLE IF EXISTS `scorm_scos`;
+DROP TABLE IF EXISTS `scorm_sco_data`;
+DROP TABLE IF EXISTS `scorm_sco_tracks`;
