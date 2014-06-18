91197b785920d70a2c6cd6f7b47359577b366f66
Christian Flothmann <cflothma@uos.de>
2014-06-18 14:38:04 +0200

    check if the package directory exists before trying to delete it

--- migrations/6_create_dynamic_packages_directory.php
+++ migrations/6_create_dynamic_packages_directory.php
@@ -41,24 +41,28 @@ class CreateDynamicPackagesDirectory extends Migration
      */
     function down()
     {
-        $files = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($this->plugin->getPackagesPath()),
-            RecursiveIteratorIterator::CHILD_FIRST
-        );
+        if (is_dir($this->plugin->getPackagesPath())) {
+            $files = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($this->plugin->getPackagesPath()),
+                RecursiveIteratorIterator::CHILD_FIRST
+            );
 
-        foreach ($files as $file) {
-            /** @var SplFileInfo $file */
-            if (in_array($file->getBasename(), array('.', '..'))) {
-                continue;
+            foreach ($files as $file) {
+                /** @var SplFileInfo $file */
+                if (in_array($file->getBasename(), array('.', '..'))) {
+                    continue;
+                }
+
+                if ($file->isFile() || $file->isLink()) {
+                    unlink($file->getRealPath());
+                } else if ($file->isDir()) {
+                    rmdir($file->getRealPath());
+                }
             }
 
-            if ($file->isFile() || $file->isLink()) {
-                unlink($file->getRealPath());
-            } else if ($file->isDir()) {
-                rmdir($file->getRealPath());
-            }
+            rmdir($this->plugin->getPackagesPath());
+        } else if (is_file($this->plugin->getPackagesPath()) || is_link($this->plugin->getPackagesPath())) {
+            unlink($this->plugin->getPackagesPath());
         }
-
-        rmdir($this->plugin->getPackagesPath());
     }
 }
